a86d4b01eff240df6d3d721605acef33305d2b14
Matthew D. Hall <zijistark@gmail.com>
2016-02-02 08:53:45 -0800

    defines, for now

--- EMF+SWMH/common/defines/emf-plus-swmh_defines.lua
+++ EMF+SWMH/common/defines/emf-plus-swmh_defines.lua
@@ -5,6 +5,8 @@ NDefines.NDiplomacy.DEMESNE_MAX_SIZE_KING_MULT = 4.0    -- up from 3
 NDefines.NDiplomacy.DEMESNE_MAX_SIZE_EMPEROR_MULT = 5.0 -- up from 4
 NDefines.NDiplomacy.VASSAL_LIMIT_KING_MULT = 30.0       -- up from 20
 NDefines.NDiplomacy.VASSAL_LIMIT_EMPEROR_MULT = 40.0    -- up from 30
+NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_KING_MULT = 25.0
+NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_EMPEROR_MULT = 35.0
 NDefines.NDiplomacy.MAX_PREP_INV_TARGET_HOLDINGS = 60   -- up from 40
 NDefines.NDiplomacy.MAX_PREP_INV_ATTACKER_HOLDINGS = 60 -- up from 40
 
