2d87b44078689c821377314da3c652aae6eb9b6e
Matthew D. Hall <zijistark@gmail.com>
2016-09-05 11:10:28 -0700

    undo HOSTS warscore multiplier change

--- EMF/common/defines/emf_defines.lua
+++ EMF/common/defines/emf_defines.lua
@@ -120,7 +120,6 @@ NDefines.NMilitary.LEVY_MAINTENANCE_FACTOR = 2.5                  -- changed in 
 NDefines.NMilitary.HOLDING_LEVY_SIZE_OWNER_MARTIAL_BASE = 0.75
 NDefines.NMilitary.HOLDING_LEVY_SIZE_OWNER_MARTIAL_MULT = 0.025
 NDefines.NMilitary.BATTLE_WARSCORE_DEFENDER_MULTIPLIER = 1.6
-NDefines.NMilitary.BATTLE_WARSCORE_HOSTS_MULTIPLIER = 1.25        -- from vanilla factor of 2.0 to vanilla's normal factor
 NDefines.NMilitary.MIN_LEVY_RAISE_OPINION_THRESHOLD = -50
 NDefines.NMilitary.ATTACKER_SIEGE_DAMAGE = 0
 NDefines.NMilitary.DEFENDER_SIEGE_DAMAGE = 0
