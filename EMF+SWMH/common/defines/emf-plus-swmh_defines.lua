8394ba2718dbfaee1bdcd212ef0aef968759e4e3
Matthew D. Hall <zijistark@gmail.com>
2016-09-08 19:07:46 -0700

    secret defines changes

--- EMF+SWMH/common/defines/emf-plus-swmh_defines.lua
+++ EMF+SWMH/common/defines/emf-plus-swmh_defines.lua
@@ -1,8 +1,4 @@
 -- Diplomacy
-NDefines.NDiplomacy.DEMESNE_MAX_SIZE_COUNT_MULT = 2.0   -- up from 1
-NDefines.NDiplomacy.DEMESNE_MAX_SIZE_DUKE_MULT = 2.0    -- up from 1
-NDefines.NDiplomacy.DEMESNE_MAX_SIZE_KING_MULT = 4.0    -- up from 3
-NDefines.NDiplomacy.DEMESNE_MAX_SIZE_EMPEROR_MULT = 5.0 -- up from 4
 NDefines.NDiplomacy.VASSAL_LIMIT_KING_MULT = 30.0       -- up from 20
 NDefines.NDiplomacy.VASSAL_LIMIT_EMPEROR_MULT = 40.0    -- up from 30
 NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_KING_MULT = 25.0
@@ -10,17 +6,11 @@ NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_EMPEROR_MULT = 35.0
 NDefines.NDiplomacy.MAX_PREP_INV_TARGET_HOLDINGS = 60   -- up from 40
 NDefines.NDiplomacy.MAX_PREP_INV_ATTACKER_HOLDINGS = 60 -- up from 40
 
--- Infamy
-NDefines.NInfamy.REALM_SIZE_BASE_MODIFIER = 0.8   -- down from 1
-
--- Character
-NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_TAX_MULTIPLIER = 0.7   -- up from 0.5
-
 -- Nomad
 NDefines.NNomad.MAX_POPULATION_EMPTY_HOLDING_MULTIPLIER = 1750 -- from 1250
 
 -- Military
-NDefines.NMilitary.SHATTERED_RETREAT_MAX_PROVINCES = 8
+NDefines.NMilitary.SHATTERED_RETREAT_MAX_PROVINCES = 7
 
 -- AI
 NDefines.NAI.COALITION_PROVINCE_MULTIPLIER = 0.7 -- down from 1.0
