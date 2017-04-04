0b61ced827e84dcb5ffed1d875b783f1a074240a
Matthew D. Hall <zijistark@gmail.com>
2017-04-04 14:41:51 -0700

    partial scripted effects (an automated portion)

--- src/mk_sr_stuff.py
+++ src/mk_sr_stuff.py
@@ -5,7 +5,67 @@ from pathlib import Path
 
 from localpaths import rootpath
 
-sr_modifier_path = rootpath / 'EMF/EMF/common/event_modifiers/emf_sr_modifiers.txt'
+emf_path = rootpath / 'EMF/EMF'
+sr_modifier_path = emf_path / 'common/event_modifiers/emf_sr_modifiers.txt'
+sr_effect_path = emf_path / 'common/scripted_effects/emf_sr_codegen_effects.txt'
+sr_trigger_path = emf_path / 'common/scripted_triggers/emf_sr_codegen_triggers.txt'
+
+# TODO: learn escalona's python parser interface (unless I create python bindings for my C++ parser)
+# and grab this information (e.g., religions) automatically.  learning that was slower than just
+# writing it out for now, but in the long-term, the code should change when the religion files change
+# automatically.
+
+# NOTE: religions without secret-religion cults are not included: ((hellenic_)?pagan)|hip_religion
+
+vanilla_religions = [
+	'catholic',
+	'cathar',
+	'fraticelli',
+	'waldensian',
+	'lollard',
+	'orthodox',
+	'bogomilist',
+	'monothelite',
+	'iconoclast',
+	'miaphysite',
+	'monophysite',
+	'paulician',
+	'nestorian',
+	'messalian',
+	'sunni',
+	'zikri',
+	'ibadi',
+	'kharijite',
+	'shiite',
+	'hurufi',
+	'druze',
+	'norse_pagan_reformed',
+	'norse_pagan',
+	'tengri_pagan_reformed',
+	'tengri_pagan',
+	'baltic_pagan_reformed',
+	'baltic_pagan',
+	'finnish_pagan_reformed',
+	'finnish_pagan',
+	'aztec_reformed',
+	'aztec',
+	'slavic_pagan_reformed',
+	'slavic_pagan',
+	'west_african_pagan_reformed',
+	'west_african_pagan',
+	'zun_pagan_reformed',
+	'zun_pagan',
+	'zoroastrian',
+	'mazdaki',
+	'manichean',
+	'yazidi',
+	'jewish',
+	'samaritan',
+	'karaite',
+	'hindu',
+	'buddhist',
+	'jain',
+]
 
 emf_religions = [
 	'adoptionist',
@@ -27,15 +87,17 @@ emf_religions = [
 	'mandaean',
 ]
 
-modifier_file_hdr = '''\
+all_religions = vanilla_religions + emf_religions
+
+
+def main():
+	# create extra event modifiers
+	with sr_modifier_path.open('w', encoding='cp1252') as f:
+		print('''\
 # -*- ck2.event_modifiers -*-
 
 # For available modifier icons, see: common/event_modifiers/REFERENCE_emf_modifier_icons.txt
-'''
-
-def main():
-	with sr_modifier_path.open('w', encoding='cp1252') as f:
-		print(modifier_file_hdr, file=f)
+''', file=f)
 
 		for r in emf_religions:
 			print('''\
@@ -46,8 +108,104 @@ secret_{0}_community = {{
 	}}
 }}'''.format(r), file=f)
 
+
+	# generate SR scripted triggers
+	with sr_trigger_path.open('w', encoding='cp1252') as f:
+		print('''\
+# -*- ck2.scripted_triggers -*-
+
+# These are code-generated & used as implementation for vanilla scripted triggers. Ideally, don't modify by hand.
+''', file=f)
+		print_trigger_has_any_sr_flag(f)
+
+	# generate SR scripted effects
+	with sr_effect_path.open('w', encoding='cp1252') as f:
+		print('''\
+# -*- ck2.scripted_effects -*-
+
+# These are code-generated & used as implementation for vanilla scripted effects. Ideally, don't modify by hand.
+''', file=f)
+
+		print_effect_set_sr_and_clear_its_flag(f)
+		print_effect_add_religion_char_flag(f)
+		print_effect_clr_religion_char_flag(f)
+		print_effect_event_target_old_religion_from_flag(f)
+
 	return 0
 
+def print_trigger_has_any_sr_flag(f):
+	print('''
+emf_sr_has_any_sr_flag = {
+	OR = {''', file=f)
+
+	for r in all_religions:
+		print('\t\thas_character_flag = character_was_' + r, file=f)
+
+	print('\t}\n}', file=f)
+
+
+def print_effect_set_sr_and_clear_its_flag(f):
+	print('''
+emf_sr_set_sr_and_clear_its_flag = {''', file=f)
+
+	for rel in all_religions:
+		print('''\
+	if = {{
+		limit = {{
+			OR = {{
+				has_character_flag = character_was_{0}
+				AND = {{
+					religion = {0}
+					emf_sr_has_any_sr_flag = no
+				}}
+			}}
+		}}
+		set_secret_religion = {0}
+		clr_character_flag = character_was_{0}
+		break = yes
+	}}'''.format(rel), file=f)
+
+	print('}', file=f)
+
+
+def print_effect_add_religion_char_flag(f):
+	print('''
+emf_sr_add_religion_char_flag = {''', file=f)
+
+	for rel in all_religions:
+		print('''\
+	if = {{
+		limit = {{ religion = {0} }}
+		set_character_flag = character_was_{0}
+		break = yes
+	}}'''.format(rel), file=f)
+
+	print('}', file=f)
+
+
+def print_effect_clr_religion_char_flag(f):
+	print('''
+emf_sr_clr_religion_char_flag = {''', file=f)
+
+	for rel in all_religions:
+		print('\tclr_character_flag = character_was_{}'.format(rel), file=f)
+
+	print('}', file=f)
+
+
+def print_effect_event_target_old_religion_from_flag(f):
+	print('''
+emf_sr_event_target_old_religion_from_flag = {''', file=f)
+
+	for rel in all_religions:
+		print('''\
+	if = {{
+		limit = {{ has_character_flag = character_was_{0} }}
+		random_character = {{ limit = {{ religion = {0} }} save_event_target_as = old_religion }}
+		break = yes
+	}}'''.format(rel), file=f)
+
+	print('}', file=f)
 
 if __name__ == '__main__':
 	sys.exit(main())
