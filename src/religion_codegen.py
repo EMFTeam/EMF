643ae93973258b8d0f8d8a13aefc918d2f546f25
DelnarErsike <delnar.ersike@gmail.com>
2019-09-27 02:48:26 +0200

    `religion_codegen` LF Line Endings

--- src/religion_codegen.py
+++ src/religion_codegen.py
@@ -93,12 +93,12 @@ def main():
 	new_loc = {}
 
 	# create SR community event modifiers
-	with sr_modifier_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with sr_modifier_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.event_modifiers')
 		print_modifiers_secret_community(f, loc, new_loc)
 
 	# generate SR scripted triggers
-	with sr_trigger_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with sr_trigger_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.scripted_triggers')
 		print_trigger_has_any_religion_char_flag(f)
 		print_trigger_is_in_PREVs_interesting_society(f)
@@ -111,7 +111,7 @@ def main():
 		print_trigger_old_religion_is_liege_sr(f)
 
 	# generate SR scripted effects
-	with sr_effect_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with sr_effect_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.scripted_effects')
 		print_effect_set_sr_and_clr_religion_char_flag(f)
 		print_effect_add_religion_char_flag(f)
@@ -129,33 +129,33 @@ def main():
 		print_effect_ai_try_to_join_society(f)
 
 	# generate religion scripted effects
-	with rel_effect_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with rel_effect_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.scripted_effects')
 		print_effect_calc_realm_province_religion_breakdown_of_THIS_for_ROOT(f, loc)
 
 	# generate bloodline scripted triggers
-	with bl_trigger_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with bl_trigger_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.scripted_triggers')
 		print_trigger_religion_same_as_bloodline_founder(f)
 
 	# generate bloodline scripted effects
-	with bl_effect_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with bl_effect_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.scripted_effects')
 		print_effect_set_bloodline_founder_religion_flag(f)
 
 	# generate alt. start scripted effects
-	with as_effect_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with as_effect_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.scripted_effects')
 		print_effect_randomize_relhead_title_names(f)
 		print_effect_activate_randomized_relhead_titles(f, loc)
 
 	# generate "secretly convert to this holy site's religion" decisions
-	with sr_holy_site_decisions_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with sr_holy_site_decisions_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.decisions')
 		print_decisions_secretly_convert_to_holy_site(f, loc, new_loc)
 
 
-	with sr_custom_loc_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with sr_custom_loc_path.open('w', encoding='cp1252', newline='\n') as f:
 		print_file_header(f, 'ck2.custom_loc')
 		print_custom_loc_GetTrueReligionAdjective(f, loc, new_loc)
 		print_custom_loc_GetTrueReligionAdherent(f, loc, new_loc)
@@ -164,7 +164,7 @@ def main():
 	# write default SR localisation
 	generate_default_sr_localisation(loc, new_loc)
 
-	with sr_localisation_path.open('w', encoding='cp1252', newline='\r\n') as f:
+	with sr_localisation_path.open('w', encoding='cp1252', newline='\n') as f:
 		print('#CODE;ENGLISH;FRENCH;GERMAN;;SPANISH;;;;;;;;;x', file=f)
 		for k in sorted(new_loc):
 			print('{};{};;;;;;;;;;;;;x'.format(k, new_loc[k]), file=f)
