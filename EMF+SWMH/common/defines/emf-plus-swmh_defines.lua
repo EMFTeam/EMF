051fc6f8d7cfc91eb713ac601492781fcf82e4d1
Matthew D. Hall <azrinon@gmail.com>
2019-11-18 05:16:05 -0800

    cannot be quite so large and still us PIs on E+S

--- EMF+SWMH/common/defines/emf-plus-swmh_defines.lua
+++ EMF+SWMH/common/defines/emf-plus-swmh_defines.lua
@@ -5,7 +5,6 @@ NDefines.NDiplomacy.VASSAL_LIMIT_EMPEROR_MULT = 40.0    -- up from 30
 NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_KING_MULT = 25.0
 NDefines.NDiplomacy.CONCLAVE_VASSAL_LIMIT_EMPEROR_MULT = 35.0
 NDefines.NDiplomacy.MAX_PREP_INV_TARGET_HOLDINGS = 60   -- up from 40
-NDefines.NDiplomacy.MAX_PREP_INV_ATTACKER_HOLDINGS = 60 -- up from 40
 
 -- Nomad
 NDefines.NNomad.MAX_POPULATION_EMPTY_HOLDING_MULTIPLIER = 1150 -- < v10.6: 1375; vanilla: 1000; [Monthly tax income determined by the current population]
